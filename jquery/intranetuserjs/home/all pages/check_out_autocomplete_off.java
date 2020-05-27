5b33be7d2cf85ae4d25d886274deff2c3e530362
George <georgewill1410@gmail.com>
2020-05-27 13:11:24 -0500

    Code to turn off autocomplete in check-out input in header

---
+++ jquery/intranetuserjs/home/all pages/check_out_autocomplete_off.java
@@ -0,0 +1,5 @@
+/* This turns off autocomplete in the "Check out" input box in the search header */
+/* This may no longer be necessary */
+
+ //BEGIN disable autocomplete in checkout box
+  $("#patronsearch").attr("autocomplete","off")
