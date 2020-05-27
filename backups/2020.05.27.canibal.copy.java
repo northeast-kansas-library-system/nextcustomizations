86e5893115d5fcf379db4c88571154c4420fde0c
George <georgewill1410@gmail.com>
2020-05-27 14:36:37 -0500

    Update canibal file

--- backups/2020.05.27.canibal.copy.java
+++ backups/2020.05.27.canibal.copy.java
@@ -120,10 +120,6 @@ $('#cat_additem span[id^="error"]').parent().css('text-align', 'right');
    return v.replace(/(\(|\)|\;|\!)/g,'')
   });
 
- //BEGIN re-logo the staff client
-  //All pages-url for logo https://nekls.org/wp-content/uploads/2018/03/nekls_next_logo.png
-   $("#header_search").before('<div id="newlogo" style="float: left; padding: 10px; height: 88;"><a href="/cgi-bin/koha/mainpage.pl"><img src="https://nekls.org/wp-content/uploads/2018/03/nekls_next_logo.png" height="50" width="150"><p style="font-size: 75%;">A service of NEKLS</p></a></div>');
-
 //Catalog > Details for -TITLE-//
  //BEGIN Redirect NoveList Select links to the staff client instead of the OPAC
 $("#NovelistSelect").one("mouseenter", function(e){
