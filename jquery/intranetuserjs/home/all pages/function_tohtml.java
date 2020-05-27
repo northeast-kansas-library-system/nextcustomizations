7f36e4aba14846c3c80e935747838d83c8e0a17a
George <georgewill1410@gmail.com>
2020-05-27 11:43:35 -0500

    Removed a comment from this file

--- jquery/intranetuserjs/home/all pages/function_tohtml.java
+++ jquery/intranetuserjs/home/all pages/function_tohtml.java
@@ -1,6 +1,5 @@
 /* This creates a function that renders text in HTML in places where HTML is not allowed */
 
-//Special functions
  //BEGIN Creates to html function
   $.fn.toHtml=function(){
    return $(this).html($(this).text())
